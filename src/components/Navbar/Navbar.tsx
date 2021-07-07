b076ee379a11d6ae5d9725a98228730bb30b77d5
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-07-08 07:06:53 +0800

    add: Navbar/Star

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,21 @@
 import React, {ReactElement} from 'react';
 import Container from './Container';
 import LabelText from './LabelText';
+import Row from '../common/Row';
+import GithubLogo from './GithubLogo';
+import Star from './Star';
 
 const Navbar: React.FC = (): ReactElement => {
   return (
     <Container>
       <LabelText>Graph Algorithms Visualizer</LabelText>
+      <Row justifyContent="center">
+        <GithubLogo></GithubLogo>
+        <Star></Star>
+      </Row>
     </Container>
   );
 };
 
 export default Navbar;
-
 
