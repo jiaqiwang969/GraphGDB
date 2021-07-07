ef457cf3ccea2affe40c3ffb3a4262ac60766af2
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-07-08 06:47:30 +0800

    add: Navbar/LabelTex

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React, {ReactElement} from 'react';
 import Container from './Container';
+import LabelText from './LabelText';
 
 const Navbar: React.FC = (): ReactElement => {
   return (
     <Container>
-      <div></div>
+      <LabelText>Graph Algorithms Visualizer</LabelText>
     </Container>
   );
 };
 
 export default Navbar;
+
 
