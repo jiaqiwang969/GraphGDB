4d31dbc4132cc5e29ffbdbb62ff7d2eaed5723ce
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-07-08 07:50:10 +0800

    add: GraphCanvs/Conatiner

---
+++ src/components/GraphCanvas/GraphCanvas.tsx
@@ -0,0 +1,8 @@
+import React, {ReactElement} from 'react';
+import Container from './Container';
+
+const GraphCanvas: React.FC = (): ReactElement => {
+  return <Container></Container>;
+};
+
+export default GraphCanvas;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement} from 'react';
 import Navbar from '../Navbar/Navbar';
-import SideNav from '../SideNav/SideNav';
+import GraphCanvas from '../GraphCanvas/GraphCanvas';
 
 interface HomeProps {
   changeTheme: Function;
@@ -10,7 +10,7 @@ const Home: React.FC<HomeProps> = (props: HomeProps): ReactElement => {
   return (
     <div>
       <Navbar changeTheme={props.changeTheme}></Navbar>
-      <SideNav />
+      <GraphCanvas></GraphCanvas>
     </div>
   );
 };
